7000e52abab5cf9127888b0fa86774b0246e302f
Alisa <alisa@example.com>
2025-05-26 21:14:03 +0400

    project-structure

---
+++ internal/config/config.go
@@ -0,0 +1,34 @@
+package config
+
+import (
+	"github.com/ilyakaznacheev/cleanenv"
+	"log"
+)
+
+type Config struct {
+	Service  Service
+	Metrics  Metrics
+	Platform Platform
+}
+
+type Service struct {
+	Port string `env:"MATERIALS_SERVICE_PORT"`
+}
+
+type Metrics struct {
+	Host string `env:"GRAFANA_HOST"`
+	Port int    `env:"GRAFANA_PORT"`
+}
+
+type Platform struct {
+	Env string `env:"ENV"`
+}
+
+func MustLoad() *Config {
+	cfg := &Config{}
+	err := cleanenv.ReadEnv(cfg)
+	if err != nil {
+		log.Fatalf("failed to read env variables: %s", err)
+	}
+	return cfg
+}
