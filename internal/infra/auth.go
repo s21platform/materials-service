8c73383556aa57f4344edd504d6e951b285808ab
romchik <roman.salimow2018@yandex.ru>
2025-09-07 16:52:49 +0300

    fix GRPC interceptor in auth

--- internal/infra/auth.go
+++ internal/infra/auth.go
@@ -8,6 +8,9 @@ import (
 
 	"github.com/s21platform/materials-service/internal/config"
 	"google.golang.org/grpc"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/metadata"
+	"google.golang.org/grpc/status"
 )
 
 func AuthInterceptorGRPC(
@@ -15,7 +18,17 @@ func AuthInterceptorGRPC(
 	req interface{},
 	_ *grpc.UnaryServerInfo,
 	handler grpc.UnaryHandler) (interface{}, error) {
-	// todo добавить логику обработки межсервисного токена
+	md, ok := metadata.FromIncomingContext(ctx)
+	if !ok {
+		return nil, status.Error(codes.Unauthenticated, "no info in metadata")
+	}
+
+	userIDs, ok := md["uuid"]
+	if !ok || len(userIDs) != 1 {
+		return nil, status.Errorf(codes.Unauthenticated, "no uuid or more than one in metadata")
+	}
+
+	ctx = context.WithValue(ctx, config.KeyUUID, userIDs[0])
 
 	return handler(ctx, req)
 }
